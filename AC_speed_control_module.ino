93aa0d89433d50d3e3d245d4b574adc7116b9d8f
Polycarp Peter <polycarp.innov8@gmail.com>
2025-02-14 10:02:46 +0100

    There was a problem, I think the TRIAC got damaged
    thid is to test the frequency

--- AC_speed_control_module.ino
+++ AC_speed_control_module.ino
@@ -1,7 +1,8 @@
 volatile bool ZC = false;
+const int led_pin = 11;
 #define OUTPUT_CONTROL A0
 #define INTERRUPT_PIN 2
-#define MAXIMUM_VALUE 3000
+#define MAXIMUM_VALUE 4000
 #define MINIMUM_VALUE 300
 #define TRIAC_CONTROL 5
 int frequency_counter;
@@ -41,7 +42,7 @@ read_from_analog = analogRead(OUTPUT_CONTROL);
 
   uint16_t calibrated_value = map(read_from_analog, 0, 872, MINIMUM_VALUE, MAXIMUM_VALUE);
 
-    calibrated_value = constrain(calibrated_value, 300, 4000);
+    calibrated_value = constrain(calibrated_value, MINIMUM_VALUE, MAXIMUM_VALUE);
 
   return calibrated_value;
 }
@@ -50,24 +51,34 @@ void setup() {
   Serial.begin(115200);
   pinMode(OUTPUT_CONTROL, INPUT);
   pinMode(TRIAC_CONTROL, OUTPUT);
+  pinMode(led_pin, OUTPUT);
   attachInterrupt(digitalPinToInterrupt(2), zeroCrossingInterruptHandler, RISING);  // Attach interrupt for pin 2
+  digitalWrite(led_pin, HIGH);
 
   Serial.println("Setup complete, waiting for zero-crossing...");
 }
 
 void loop() {
 
-  uint16_t motor_control = outputControlKnob();
-  
+int frequency = frequencyCounter();
 
-   if (ZC) {
-     Serial.println("zero-crossing Occured...");
+ if(frequency != -1) Serial.println("The frequency is: " + String(frequency));
+
+  //uint16_t motor_control = outputControlKnob();
+
+ 
+  
+  //Serial.println("Motor control value:" + String(motor_control));
+
+  /* if (ZC) {
+     //Serial.println("zero-crossing Occured...");
     delayMicroseconds(motor_control);
     digitalWrite(TRIAC_CONTROL, HIGH);
     delayMicroseconds(250);
     digitalWrite(TRIAC_CONTROL, LOW);
     ZC = false;
-  }
+
+  }*/
 
 
 
